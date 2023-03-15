7ecdebae6f2e2a6b9314bf6941b31376666eb5be
geiselrocha <geiselrocha@hotmail.com>
2023-03-14 23:15:43 -0300

    feat: implements process function on file_process module

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(instance.__len__()):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+    
+    file_line = txt_importer(path_file)
+    
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_line),
+        "linhas_do_arquivo": file_line,
+    }
+    instance.enqueue(file_info)
+    print(file_info, file=sys.stdout)
 
 
 def remove(instance):
