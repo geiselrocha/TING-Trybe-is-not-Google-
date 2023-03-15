1eb3f8aa1a9510e1f6b8f715f55c9a9d0b8d8571
geiselrocha <geiselrocha@hotmail.com>
2023-03-15 12:49:47 -0300

    feat: implements exists_word function on word_search module

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    
+    for i in range(len(instance)):
+        data = instance.search(i)
+        file_line = data["linhas_do_arquivo"]
+        occurrences = []
+
+        for j in range(len(file_line)):
+            if word.lower() in file_line[j].lower():
+                occurrences.append(j + 1)
+    
+        if len(occurrences):
+            result.append({
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": [{"linha": row} for row in occurrences],
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
